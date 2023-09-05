070613585b50e40a62defed1e611002c3f730d99
david <palacingimenezdavid.fpllefia@gmail.com>
2023-09-05 16:26:16 +0200

    call api using https

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -4,19 +4,19 @@ export const WeatherService = {}
 
 // return the weather of today by location
 WeatherService.getWeatherByLocation = async (location) => {
-  const result = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API}&q=${location}&lang=es
+  const result = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API}&q=${location}&lang=es
 `);
   return result.data;
 }
 
 // return the list of locations that match the value
 WeatherService.getAutocompleteLocations = async (value) => {
-  const result = await axios.get(`http://api.weatherapi.com/v1/search.json?key=0a945511d26249ca8a4141626232808&q=${value}`);
+  const result = await axios.get(`https://api.weatherapi.com/v1/search.json?key=0a945511d26249ca8a4141626232808&q=${value}`);
   return result.data;
 }
 
 // return the forecast of given days (0-14) by location
 WeatherService.getWeatherByLocationAndDay = async (location, day) => {
-  const result = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API}&q=${location}&days=${day}&lang=es`);
+  const result = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API}&q=${location}&days=${day}&lang=es`);
   return result.data;
 }
